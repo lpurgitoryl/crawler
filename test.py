7063c5e95430bc4774fae15af249c7fe90d68cc7
Nicole Garcia <lpurgitoryl@gmail.com>
2023-05-01 17:19:14 -0700

    jupyte files to see text and grabbing top hot posts

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
 # DOCS
 # https://praw.readthedocs.io/en/stable/getting_started/quick_start.html#submission-iteration
 # https://praw.readthedocs.io/en/stable/tutorials/comments.html
-
+# https://stackoverflow.com/questions/4041238/why-use-def-main
+# https://towardsdatascience.com/scraping-reddit-data-1c0af3040768
+import pandas as pd
 import praw
-
 from json import dumps, JSONEncoder
 
 # To create a read-only Reddit instance, you need three pieces of information:
 # Client ID
 # Client secret
 # User agent
-import os
-print(os.getcwd())
 
 reddit = praw.Reddit(client_id="3IS_PPpIX3IkVIh-1f8cHQ",
 client_secret="AvAKHMywUgLyGlVSVD0bQMRpZbhb1w",
 user_agent ="crawler/scrapper")
-
-
 
 
 url = "https://www.reddit.com/r/funny/comments/3g1jfi/buttons/"
@@ -27,10 +24,23 @@ submission = reddit.submission(url=url)
 # submission = reddit.submission("3g1jfi")
 
 
-submission.comments.replace_more(limit=0)
-for top_level_comment in submission.comments:
-    print(top_level_comment.body)
+# submission.comments.replace_more(limit=0)
+# for top_level_comment in submission.comments:
+#     print(top_level_comment.body)
+
+# get 10 hot posts from the MachineLearning subreddit
+hot_posts = reddit.subreddit('MachineLearning').hot(limit=10)
+for post in hot_posts:
+    print(post.title)
     
+# import pandas as pd
+# posts = []
+# ml_subreddit = reddit.subreddit('MachineLearning')
+# for post in ml_subreddit.hot(limit=10):
+#     posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created])
+# posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
+# print(posts)
+
 # # assume you have a Subreddit instance bound to variable `subreddit`
 # for submission in subreddit.hot(limit=10):
 #     print(submission.title)
