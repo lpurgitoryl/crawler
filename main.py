410d54dededff1bb1b6542079c2f56e5505ff6a7
akasakakona <tomliang20011018@gmail.com>
2023-05-05 10:54:13 -0700

    Added parser

--- main.py
+++ main.py
@@ -2,14 +2,9 @@ import praw
 import pandas as pd
 import os
 import json
-import pprint
+import re
 
-
-    
-# Using the special variable 
-# __name__
-if __name__=="__main__":
-    
+def getCredential():
     if os.path.isfile('credentials.json'):
         with open('credentials.json') as f:
             data = json.load(f)
@@ -26,8 +21,44 @@ if __name__=="__main__":
         data['user_agent'] = user_agent
         with open('credentials.json', 'w') as outfile:
             json.dump(data, outfile)
+    return praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
-    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
+def parseLinks(md):
+    """ Return dict of links in markdown """
+    INLINE_LINK_RE = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
+    FOOTNOTE_LINK_URL_RE = re.compile(r'\[(\d+)\]:\s+(\S+)')
+    IS_LINK_RE = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    links = []
+
+    for _, link in INLINE_LINK_RE.findall(md) + FOOTNOTE_LINK_URL_RE.findall(md):
+        if IS_LINK_RE.match(link):
+            links.append(link)
+
+    return links
+
+
+def crawlBySubreddit(subreddit_name):
+    posts = []
+    urls = []
+    subreddit = reddit.subreddit(subreddit_name)
+    for post in subreddit.hot(limit=10):
+        posts.append([post.title, post.score, post.id, post.url, post.num_comments, post.selftext, post.created])
+    #We also probably want author, upvote ratio, comments, and urls included in each post
+    df = pd.DataFrame(posts, columns=['title', 'score', 'id', 'url', 'num_comments', 'body', 'created', 'author', 'upvote_ratio', 'comments', 'urls'])
+    json_info = df.to_json(orient='records', indent=4)
+    print(json_info)
+    
+# Using the special variable 
+# __name__
+if __name__=="__main__":
+
+    reddit = getCredential()
     
     init = input('\nHello, Welcome to r\'Crawler\n\nPlease select an NUMBER option\n TODO PUT MENU LIST HERE\n\n')
     tempList = []
@@ -35,13 +66,7 @@ if __name__=="__main__":
         match init:
             case '1':
                 subreddit_name = input("\ninput just the subreddit name\n Ex. if you want \"r/MachineLearning\" only input \"MachineLearning\"\n")
-                posts = []
-                subreddit = reddit.subreddit(subreddit_name)
-                for post in subreddit.hot(limit=10):
-                    posts.append([post.title, post.score, post.id, post.url, post.num_comments, post.selftext, post.created])
-                df = pd.DataFrame(posts, columns=['title', 'score', 'id', 'url', 'num_comments', 'body', 'created'])
-                json_info = df.to_json(orient='records', indent=4)
-                print(json_info)
+                crawlBySubreddit(subreddit_name)
 
             case '2':
                 print("2")
