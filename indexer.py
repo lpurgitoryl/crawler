dd08ffd481a6da1ad08bcb83e77ea96819c92560
akasakakona <akasakakona@pm.me>
2023-06-10 01:22:29 -0700

    update .gitignore & indexer done

--- indexer.py
+++ indexer.py
@@ -7,7 +7,7 @@ from org.apache.lucene.analysis.standard import StandardAnalyzer
 from org.apache.lucene.document import Document, Field, FieldType
 from org.apache.lucene.index import \
     FieldInfo, IndexWriter, IndexWriterConfig, IndexOptions
-from org.apache.lucene.store import NIOFSDirectory
+from org.apache.lucene.store import NIOFSDirectory , SimpleFSDirectory
 
 
 import json
@@ -20,7 +20,15 @@ if len(sys.argv) != 3:
 
 data_dir = sys.argv[1]
 index_dir = sys.argv[2]
+
+if not os.path.isdir(data_dir):
+    print("Error: data_dir is not a directory")
+    exit(1)
+if not os.path.isdir(index_dir):
+    os.mkdir(index_dir)
+
 data = []
+
 try:
     for filename in os.listdir(data_dir):
         if filename.endswith(".json"):
@@ -32,35 +40,48 @@ except:
     print("Error: Invalid JSON")
     exit(1)
 
-lucene.initVM()
-store = NIOFSDirectory.open(Paths.get(index_dir))
+lucene.initVM(vmargs=['-Djava.awt.headless=true'])
+
+store = SimpleFSDirectory(Paths.get(index_dir))
 analyzer = StandardAnalyzer()
 config = IndexWriterConfig(analyzer)
 config.setOpenMode(IndexWriterConfig.OpenMode.CREATE)
 writer = IndexWriter(store, config)
+
+#define a few field types here
+#should it be indexed? should it be tokenized? should it be stored?
+#stored means that the field will be returned in the search results as it is stored in the index
+#tokenized means that the field will be split into tokens
+#indexed means that the field will be searchable
+
+#here are all the fields we will be using
+"""
+title: indexed, tokenized, and stored
+author: indexed, not tokenized, and stored
+id: indexed, not tokenized, and not stored
+body: indexed, tokenized, and stored //not a good practice to store body in index, but we will do it for this project
+subreddit: indexed, not tokenized, and not stored
+"""
+type0 = FieldType()
+type0.setStored(True)
+type0.setTokenized(True)
+#stores document, term frequency, and position
+type0.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS)
+
+type1 = FieldType()
+type1.setStored(True)
+type1.setTokenized(False)
+type1.setIndexOptions(IndexOptions.DOCS)
+
+print("Indexing...")
 for doc in data:
     document = Document()
-    metaType = FieldType()
-    metaType.setStored(True)
-    metaType.setTokenized(True)
-    document.add(Field("title", doc["title"], metaType))
-    metaType.setStored(True)
-    metaType.setTokenized(False)
-    document.add(Field("score", str(doc["score"]), metaType))
-    metaType.setStored(True)
-    document.add(Field("id", doc["id"], metaType))
-    document.add(Field("url", doc["url"], metaType))
-    document.add(Field("num_comments", str(doc["num_comments"]), metaType))
-    metaType.setStored(True)
-    metaType.setTokenized(True)
-    # metaType.setIndexOptions(IndexOptions.DOCS_AND_FREQS_AND_POSITIONS_AND_OFFSETS)
-    document.add(Field("body", doc["body"], metaType))
-    metaType.setStored(True)
-    metaType.setTokenized(False)
-    document.add(Field("created", str(doc["created"]), metaType))
-    # metaType.setStored(True)
-    # document.add(Field("links", , metaType))
-    # metaType.setStored(True)
-    # metaType.setTokenized(True)
-    # document.add(Field("comments", " ".join(doc["comments"]),  metaType))
+    document.add(Field("title", doc["title"], type0))
+    document.add(Field("id", doc["id"], type1))
+    document.add(Field("body", doc["body"], type0))
+    document.add(Field("subreddit", doc["subreddit"], type1))
+    document.add(Field("author", doc["author"], type1))
     writer.addDocument(document)
+writer.commit()
+writer.close()
+print("Done indexing!")
